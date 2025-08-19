9bd2f4f4b864f5c0935945ecc87c9b4ede90bc14
Chase Núñez <chase.nunez@eawag.ch>
2025-08-19 14:23:04 +0000

    STABLE: example working nominally

--- frictionless.py
+++
@@ -1,7 +0,0 @@
-from pprint import pprint
-from frictionless import Checklist, validate
-
-checklist = Checklist()
-pprint(checklist.scope)
-report = validate('capital-invalid.csv')  # we don't pass the checklist as the empty one is default
-pprint(report.flatten(['type', 'message']))
